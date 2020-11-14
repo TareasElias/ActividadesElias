75439e53ed8925c47283049ff48ae16a095147b7
EJElias <ej117258@gmail.com>
2020-11-14 14:32:22 -0600

    Practica 1 MVC
    Practica 1 MCV

---
+++ IntroduccionMVC/IntroduccionMVC/Controllers/OperacionesController.cs
@@ -0,0 +1,68 @@
+﻿using IntroduccionMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntroduccionMVC.Controllers
+{
+    public class OperacionesController : Controller
+    {
+        // GET: Operaciones
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult indexMultiplicar()
+        {
+            return View();
+        }
+        public ActionResult indexDividir()
+        {
+            return View();
+        }
+        public ActionResult Suma()
+        {
+            Datos objmodelo = new Datos();
+            objmodelo.variableA = 3;
+            objmodelo.variableB = 4;
+            objmodelo.resultado = objmodelo.variableA + objmodelo.variableB;
+            ViewData["resultado"] = objmodelo.resultado;
+
+            return View();
+        }
+        public ActionResult Resta()
+        {
+            Datos objmodelo = new Datos();
+            objmodelo.variableA = double.Parse(Request.Form["txtnum1"].ToString());
+            objmodelo.variableB= double.Parse(Request.Form["txtnum2"].ToString());
+            objmodelo.resultado = objmodelo.variableA - objmodelo.variableB;
+            return View("Resta",objmodelo);
+        }
+
+        public ActionResult Multi()
+        {
+            Datos objmodelo = new Datos();
+            objmodelo.variableA = double.Parse(Request.Form["txtnum1"].ToString());
+            objmodelo.variableB = double.Parse(Request.Form["txtnum2"].ToString());
+            objmodelo.resultado = objmodelo.variableA * objmodelo.variableB;
+            return View("Multi", objmodelo);
+        }
+        public ActionResult Divi()
+        {
+            Datos objmodelo = new Datos();
+            objmodelo.variableA = double.Parse(Request.Form["txtnum1"].ToString());
+            objmodelo.variableB = double.Parse(Request.Form["txtnum2"].ToString());
+            try{
+                objmodelo.resultado = objmodelo.variableA / objmodelo.variableB;
+            }
+            catch( Exception ex)
+            {
+                
+            }
+           
+            return View("Divi", objmodelo);
+        }
+    }
+}
---
+++ IntroduccionMVC/IntroduccionMVC/Models/Datos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IntroduccionMVC.Models
+{
+    public class Datos
+    {
+        public double variableA { get; set; }
+        public double variableB { get; set; }
+        public double resultado { get; set; }
+
+
+    }
+}
